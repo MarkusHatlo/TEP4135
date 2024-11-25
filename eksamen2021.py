8e8c9540d96c627060b337b937997cbd107e2bc9
Markus Hatlo <marhat182@gmail.com>
2024-11-25 10:35:53 +0100

    Ferdig 2021

--- eksamen2021.py
+++ eksamen2021.py
@@ -1,21 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-S_0 = 0.02
-n = 0.0145
-A = 0.39
-Pe = 1.57
-Rh = 0.346
-tol = 0.001
-test = 1
-y_t = 0.28
 
-print('Start')
-A_new = A + 0.1*y_t
-Pe_new = Pe + y_t*2*0.1
-Q = A_new*(1/n)*(A_new/Pe_new)**(2/3)*S_0**(1/2)
+# S_0 = 0.02
+# n = 0.0145
+# A = 0.39
+# Pe = 1.57
+# Rh = 0.346
+# tol = 0.001
+# test = 1
+# y_t = 0.28
 
-print(f'Dette er Q = {Q}')
-print(f'Dette er y = {y_t}')
+# print('Start')
+# A_new = A + 0.1*y_t
+# Pe_new = Pe + y_t*2*0.1
+# Q = A_new*(1/n)*(A_new/Pe_new)**(2/3)*S_0**(1/2)
+
+# print(f'Dette er Q = {Q}')
+# print(f'Dette er y = {y_t}')
 
 # while test > tol:
 #     A_new = A + 0.1*y
@@ -33,3 +35,20 @@ print(f'Dette er y = {y_t}')
 #         svar = Q
 #         print(f'Dette er Q = {svar}')
 #         print(f'Dette er y = {y}')
+
+x = np.linspace(-3,3,100)
+y = np.linspace(-0.99,0.99,100)
+x,y = np.meshgrid(x,y)
+
+u = 1
+a = 0.5
+t = 0.3
+h = (1+(2*a**2)/(u*(1-t**2)))
+
+phi = u*y - a**2/np.sqrt(x**2+(y-1)**2)*np.sin(np.arctan2(y-1,x)) - a**2/np.sqrt(x**2+(y+1)**2)*np.sin(np.arctan2(y+1,x))
+
+_,f1 = plt.subplots()
+
+f1.contour(x,y,phi, levels = np.linspace(-1,1,20))
+f1.contour(x,y,phi, levels = [-t,t],colors = 'black')
+plt.show()
